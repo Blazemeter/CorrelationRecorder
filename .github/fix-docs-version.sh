1e280ab0f0b136ac53fb9ab5cb1496e3595954c6
David <dgiordano@perforce.com>
2023-04-13 20:20:18 -0300

    Check for changes before commit and push

--- .github/fix-docs-version.sh
+++ .github/fix-docs-version.sh
@@ -15,7 +15,9 @@ update_file_versions() {
 update_file_versions ${VERSION} README.md
 
 git add README.md
-git config --local user.email "$(git log --format='%ae' HEAD^!)"
-git config --local user.name "$(git log --format='%an' HEAD^!)"
-git commit -m "[skip ci] Updated README version"
-git push origin HEAD:master
+if ! git diff-index --quiet HEAD; then
+  git config --local user.email "$(git log --format='%ae' HEAD^!)"
+  git config --local user.name "$(git log --format='%an' HEAD^!)"
+  git commit -m "[skip ci] Updated README version"
+  git push origin HEAD:master
+fi
